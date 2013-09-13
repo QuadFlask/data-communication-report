54f3a03d14be17eea6b19508e757bacd51c0ef08
QuadFlask <pop9310@gmail.com>
2013-09-14 00:31:20 +0900

    remove dirty comment in CheckSumChecker

--- src/main/flask/codes/checker/CheckSumChecker.java
+++ src/main/flask/codes/checker/CheckSumChecker.java
@@ -21,30 +21,3 @@ public class CheckSumChecker {
 	}
 
 }
-//
-//
-//	// per 16bits
-//	public static BitPattern generate(BitPattern dataword) {
-//		int sum = 0;
-//		int loop = dataword.length() / checksumSize;
-//		int remain = dataword.length() % checksumSize;
-//
-//		for (int i = 0; i < loop; i++)
-//			sum += valueOf(dataword, remain + i * checksumSize, checksumSize);
-//		sum += valueOf(dataword, 0, remain);
-//
-//		sum %= (int) Math.pow(2, checksumSize);
-//
-//		BitPattern checksum = new BitPattern();
-//		checksum.set(sum, checksumSize);
-//
-//		return checksum;
-//	}
-//
-//	public static int valueOf(BitPattern bitPattern, int from, int length) {
-//		int value = 0;
-//		length += from;
-//		for (int i = from; i < length; i++)
-//			value += Math.pow(2, length - i - 1) * bitPattern.get(i).value();
-//		return value;
-//	}
